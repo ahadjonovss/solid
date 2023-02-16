56c0c1e7d51fba5220e9cc5f102d285e573fc0ba
Asus <samandarahadjonovss@gmail.com>
2023-02-16 15:27:52 +0500

    i done

---
+++ lib/solid/5-dependency_inversion.dart
@@ -0,0 +1,64 @@
+//D — Dependency Inversion
+
+abstract class Destroyer{
+
+  destroy();
+}
+
+
+
+class Builder extends Destroyer{
+  @override
+  destroy() {
+    print("I destroy buildings with my hands");
+  }
+
+}
+
+
+class Bulldozer extends Destroyer{
+  @override
+  destroy() {
+    print("I destroy buildings with my technique");
+  }
+}
+
+
+
+
+
+
+// second example
+
+
+abstract class Car{
+  move();
+}
+
+
+class Tesla extends Car{
+  @override
+  move() {
+    print("I use electricity to move");
+  }
+
+}
+
+
+class Gentra extends Car{
+  @override
+  move() {
+    print("I use gas and gasoline to move");
+  }
+
+}
+
+
+class BYDChaser extends Car{
+  @override
+  move() {
+    print("I use gasoline and electricity to move");
+  }
+
+}
+
