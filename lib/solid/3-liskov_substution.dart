7235c34e6a8b7a2f3714e26966960f6c65323de3
Asus <samandarahadjonovss@gmail.com>
2023-02-16 15:14:38 +0500

    l done

---
+++ lib/solid/3-liskov_substution.dart
@@ -0,0 +1,24 @@
+// L — Liskov Substitution
+
+
+class Azamat{
+
+  String name = "Azamat";
+
+  String makeCoffee(){
+    return "Here is your coffee";
+  }
+
+}
+
+
+class Azamatovich extends Azamat{
+
+  @override
+  String makeCoffee() {
+    return "My father is not there, but here is your coffee";
+  }
+
+
+}
+
